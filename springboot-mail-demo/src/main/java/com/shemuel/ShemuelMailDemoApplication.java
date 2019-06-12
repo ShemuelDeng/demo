b2678b17763839b85bbde1205ec0f3b2a87a8937
shemuel <1241916209@qq.com>
2019-06-12 13:51:37 +0800

    邮件测试

---
+++ springboot-mail-demo/src/main/java/com/shemuel/ShemuelMailDemoApplication.java
@@ -0,0 +1,13 @@
+package com.shemuel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShemuelMailDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShemuelMailDemoApplication.class, args);
+    }
+
+}
